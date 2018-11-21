a7e8f140ccdecc9fbb64d1f9a186f1bbe6a611dd
Sebastian Haderecker <sebastian.haderecker@gmail.com>
2018-11-21 23:24:56 +0100

    Update path

--- retweet_forever.sh
+++ retweet_forever.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 trap 'exit 130' INT
 while true ; do
-    # Specify the script file location
+    # Specify the script and log file location
     cd /home/pi/retweet_bot
-    python retweet.py >> retweet.log
+    python retweet.py >> /home/pi/retweet_bot/retweet.log
     sleep 2m
 done
